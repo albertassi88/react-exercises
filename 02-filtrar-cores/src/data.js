92f07247f576713317e2a1cc0de595918b314f50
albertassi88 <albertassi88@gmail.com>
2021-01-16 12:18:45 -0300

    filtrar cores

---
+++ 02-filtrar-cores/src/data.js
@@ -0,0 +1,28 @@
+const cores = [
+    {
+        name: 'preto',
+        value: '#000000'
+    },
+    {
+        name: 'azul',
+        value: '#0000FF'
+    },
+    {
+        name: 'verde',
+        value: '#008000'
+    },
+    {
+        name: 'rosa',
+        value: '#FFC0CB'
+    },
+    {
+        name: 'amarelo',
+        value: '#FFFF00'
+    },
+    {
+        name: 'vermelho',
+        value: '#FF0000'
+    },
+]
+
+export default cores;
