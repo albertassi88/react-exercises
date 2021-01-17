675e69ec87b6d24e719f610b4166df574b6d39e2
albertassi88 <albertassi88@gmail.com>
2021-01-16 21:26:31 -0300

    filtrar cores

--- 02-filtrar-cores/src/App.js
+++ 02-filtrar-cores/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import ArrayCores from './ArrayCores';
+import cores from './data';
+import './App.css';
 
 class App extends React.Component {
-  render() {    
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: ""
+    }
+  }
+
+  hundleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  render() {  
+    const { inputValue } = this.state;  
     return (
-      <ArrayCores />
+      <main>
+        <input type="text" onChange={this.hundleChange}/>
+        <ul>
+          {cores.filter(cor => cor.name.includes(inputValue)).map(cor => 
+            <li key={cor.value}>
+              <div style={{ backgroundColor: cor.value }} className="cor-square"/>
+              {cor.name}
+            </li>)}
+        </ul>
+      </main>
     )   
   }
 }
